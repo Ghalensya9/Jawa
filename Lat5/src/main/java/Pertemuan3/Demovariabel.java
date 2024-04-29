16e67c6cc5fc22b84ce711f503f5959dec208acc
A-4 <A-4@A-4>
2024-04-29 11:38:48 +0700

    Holla

---
+++ Lat5/src/main/java/Pertemuan3/Demovariabel.java
@@ -0,0 +1,38 @@
+
+public class DemoVariabel {
+
+    public static void main(String[] args) {
+// Deklarasi variabel
+
+        char huruf;
+        boolean isMasihBelajar;
+        int nilaiUjian;
+        double hargaBarang;
+        float diskon;
+        byte umur;
+        short ghalen;
+        long populasiNegara;
+        String namaLengkap;
+// Inisialisasi variabel
+        huruf = 'a';
+        isMasihBelajar = true;
+        nilaiUjian = 80;
+        hargaBarang = 150000.0;
+        diskon = 0.1f;
+        umur = 17;
+        ghalen = 5121;
+        populasiNegara = 273523615;
+        namaLengkap = "Budi Raharjo";
+// Menampilkan data
+        System.out.println("Data:");
+        System.out.println("Huruf: " + huruf);
+        System.out.println("Masih belajar: " + isMasihBelajar);
+        System.out.println("Nilai ujian: " + nilaiUjian);
+        System.out.println("Harga barang: " + hargaBarang);
+        System.out.println("Diskon: " + diskon);
+        System.out.println("Umur: " + umur);
+        System.out.println("Kode pos: " + ghalen);
+        System.out.println("Populasi negara: " + populasiNegara);
+        System.out.println("Nama lengkap: " + namaLengkap);
+    }
+}
